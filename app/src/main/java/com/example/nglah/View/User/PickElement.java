b3ad1af39276b6954d7f9217c4515de7b95594e8
MohamedAshraf <34422608+MohamedAshrafMahmoud@users.noreply.github.com>
2019-07-09 17:44:48 +0200

    mohamed  9/jul

--- app/src/main/java/com/example/nglah/View/User/PickElement.java
+++ app/src/main/java/com/example/nglah/View/User/PickElement.java
@@ -1,15 +1,15 @@
-package com.example.nglah.View;
+package com.example.nglah.View.User;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nglah.R;
 
-public class ChooseActions extends AppCompatActivity {
+public class PickElement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_actions);
+        setContentView(R.layout.activity_pick_element);
     }
 }
--- app/src/main/java/com/example/nglah/View/User/UserInfo.java
+++ app/src/main/java/com/example/nglah/View/User/UserInfo.java
@@ -1,15 +1,15 @@
-package com.example.nglah.View.Driver;
+package com.example.nglah.View.User;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nglah.R;
 
-public class TransportInfo extends AppCompatActivity {
+public class UserInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transport_info);
+        setContentView(R.layout.activity_user_info);
     }
 }
