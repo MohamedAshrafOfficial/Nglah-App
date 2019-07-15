7f1673903e765b3d065788e125d1f3fb1825ed2a
PRO_Abdo <abdrahmanomran1997@gmail.com>
2019-07-15 19:44:48 +0200

    Abdo Update

--- app/src/main/java/com/example/nglah/View/SignIn.java
+++ app/src/main/java/com/example/nglah/View/SignIn.java
@@ -2,14 +2,181 @@ package com.example.nglah.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.nglah.AbdoCode.ApiClient;
+import com.example.nglah.AbdoCode.JsonPlaceHolderApi;
+import com.example.nglah.AbdoCode.Model.DriverModel.LoginDriverService;
+import com.example.nglah.AbdoCode.Model.DriverModel.Driver;
+import com.example.nglah.AbdoCode.Model.NglahModel.LoginNglahService;
+import com.example.nglah.AbdoCode.Model.NglahModel.Nglah;
 import com.example.nglah.R;
 
-public class SignIn extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SignIn extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private Spinner spinnerUser;
+
+    private boolean flagUserSelected;
+
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // init all widgets in this activity
+        initWidgets();
+
+        // get api client to connect server
+        jsonPlaceHolderApi = ApiClient.getApiClient().create(JsonPlaceHolderApi.class);
+
+        // init spinner and selected user login
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("صاحب نقله");
+        arrayList.add("صاحب سيارة");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrayList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUser.setAdapter(adapter);
+        spinnerUser.setOnItemSelectedListener(this);
+
+        // action all widgets in this activity
+        actionWidgets();
+    }
+
+    private void initWidgets(){
+        emailEditText = findViewById(R.id.et_user_name);
+        passwordEditText = findViewById(R.id.et_password);
+        loginButton = findViewById(R.id.bt_login);
+
+        spinnerUser = findViewById(R.id.sp_user);
+    }
+
+
+    private void actionWidgets(){
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (flagUserSelected){
+
+                    nglahLogin(email, password);
+
+                }else{
+
+                    driverLogin(email, password);
+                }
+
+            }
+        });
+    }
+
+
+    private void driverLogin(String email, String password){
+
+        if (email.isEmpty() && password.isEmpty()){
+            return;
+        }
+
+        Call<LoginDriverService> call = jsonPlaceHolderApi.getDriverInfo(email, password);
+
+        call.enqueue(new Callback<LoginDriverService>() {
+            @Override
+            public void onResponse(Call<LoginDriverService> call, Response<LoginDriverService> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(SignIn.this, "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                LoginDriverService driverResponse = response.body();
+
+                if (driverResponse.isSuccess()) {
+
+                    List<Driver> driver = driverResponse.getDriverInfo();
+
+                    Toast.makeText(SignIn.this, "Welcome Mr/Miss "
+                            + driver.get(0).getFirstName() + " " + driver.get(0).getLastName(), Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignIn.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginDriverService> call, Throwable t) {
+                Toast.makeText(SignIn.this, "User Name or Password is Invalid", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void nglahLogin(String email, String password){
+
+        if (email.isEmpty() && password.isEmpty()){
+            return;
+        }
+
+        Call<LoginNglahService> call = jsonPlaceHolderApi.getNglahInfo(email, password);
+
+        call.enqueue(new Callback<LoginNglahService>() {
+            @Override
+            public void onResponse(Call<LoginNglahService> call, Response<LoginNglahService> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(SignIn.this, "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                LoginNglahService nglahResponse = response.body();
+
+                if (nglahResponse.isSuccess()) {
+
+                    List<Nglah> nglah = nglahResponse.getNglahOwnerInfo();
+
+                    Toast.makeText(SignIn.this, "Welcome Mr/Miss "
+                            + nglah.get(0).getFirstName() + " " + nglah.get(0).getLastName(), Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignIn.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginNglahService> call, Throwable t) {
+                Toast.makeText(SignIn.this, "User Name or Password is Invalid", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        if (position == 0)
+            flagUserSelected = true;     // nglah owner
+        else
+            flagUserSelected = false;    // car owner
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- app/src/main/java/com/example/nglah/View/User/PickPlace_Inside.java
+++ app/src/main/java/com/example/nglah/View/User/PickPlace_Inside.java
@@ -2,14 +2,137 @@ package com.example.nglah.View.User;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.nglah.AbdoCode.Model.Database.DB;
 import com.example.nglah.R;
 
-public class PickPlace_Inside extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class PickPlace_Inside extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinnerCountry, spinnerRegion, spinnerCity, spinnerSector;
+    DB dbobject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_place__inside);
+
+        // init all widgets in this activity
+        initWidgets();
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("- اختار -");
+        arrayList.add("السعوديه");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrayList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountry.setAdapter(adapter);
+
+        spinnerCountry.setOnItemSelectedListener(this);
+        spinnerRegion.setOnItemSelectedListener(this);
+        spinnerCity.setOnItemSelectedListener(this);
+        spinnerSector.setOnItemSelectedListener(this);
+
+        spinnerRegion.setEnabled(false);
+        spinnerCity.setEnabled(false);
+        spinnerSector.setEnabled(false);
     }
+
+
+    public void initWidgets(){
+
+        dbobject = new DB(this);
+
+        spinnerCountry = findViewById(R.id.sp_country);
+        spinnerRegion = findViewById(R.id.sp_region);
+        spinnerCity = findViewById(R.id.sp_city);
+        spinnerSector = findViewById(R.id.sp_sector);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+
+        switch (adapterView.getId()){
+
+            case R.id.sp_country:
+                String text = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(this, ""+text, Toast.LENGTH_SHORT).show();
+
+                if (!"- اختار -".equals(text)){
+
+                    Map<String, ArrayList> data = dbobject.getRegionsWithSectors(String.valueOf(position));
+
+                    ArrayAdapter<String> regionAdapter = new ArrayAdapter<String>(this,
+                            android.R.layout.simple_spinner_item, data.get("regionList"));
+
+                    ArrayAdapter<String> sectorAdapter = new ArrayAdapter<String>(this,
+                            android.R.layout.simple_spinner_item, removeRepeatedValues(data.get("sectorList")));
+
+                    regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sectorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                    spinnerRegion.setAdapter(regionAdapter);
+                    spinnerSector.setAdapter(sectorAdapter);
+
+                    spinnerRegion.setEnabled(true);
+                    spinnerSector.setEnabled(true);
+                }
+
+                break;
+
+            case R.id.sp_region:
+                text = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(this, ""+text, Toast.LENGTH_SHORT).show();
+
+                ArrayAdapter<String> cityAdapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_item, dbobject.getCities(String.valueOf(position+1)));
+
+                cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                spinnerCity.setAdapter(cityAdapter);
+
+                spinnerCity.setEnabled(true);
+
+                break;
+
+            case R.id.sp_city:
+                text = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(this, ""+text, Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.sp_sector:
+                text = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(this, ""+text, Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                Toast.makeText(this, "Nothing", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private ArrayList removeRepeatedValues(ArrayList list){
+        ArrayList arrayList = list;
+        Set set = new HashSet(arrayList);
+        arrayList.clear();
+        arrayList.addAll(set);
+        return arrayList;
+    }
+
 }
