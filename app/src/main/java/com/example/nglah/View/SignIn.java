188f5bd435022a0a57c1f6925d9d50e26761bf7b
hassan <hassanelkhadrawy59@gmail.com>
2019-07-19 19:56:46 +0200

    hassan 7:56

--- app/src/main/java/com/example/nglah/View/SignIn.java
+++ app/src/main/java/com/example/nglah/View/SignIn.java
@@ -2,14 +2,19 @@ package com.example.nglah.View;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
  import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nglah.Services.ApiClient;
@@ -57,9 +62,17 @@ public class SignIn extends AppCompatActivity implements AdapterView.OnItemSelec
         arrayList.add("صاحب سيارة");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arrayList);
+                android.R.layout.simple_list_item_1, arrayList) {
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView tv = view.findViewById(android.R.id.text1);
+                tv.setTextColor(Color.WHITE);
+                return view;
+            }
+        };
         spinnerUser.setAdapter(adapter);
         spinnerUser.setOnItemSelectedListener(this);
         sharedPreferences=getSharedPreferences("nglah_file",MODE_PRIVATE);
